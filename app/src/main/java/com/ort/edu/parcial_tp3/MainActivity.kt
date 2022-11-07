af6d91b577a649d2d978cfb33dedc3e6f244b141
sofisrur <sofisrur1@gmail.com>
2022-11-07 14:27:03 -0300

    nav costado y set

--- app/src/main/java/com/ort/edu/parcial_tp3/MainActivity.kt
+++ app/src/main/java/com/ort/edu/parcial_tp3/MainActivity.kt
@@ -3,46 +3,58 @@ package com.ort.edu.parcial_tp3
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private var userName: String? = null
 
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupBottomNavigationView()
+        // Busco y guardo la referencia a las vistas en variables
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        setupDrawerLayout()
     }
 
-    /**
-     * Configuro la barra inferior de navegación
-     */
-    private fun setupBottomNavigationView() {
-        // Busco los componentes en la View generada por su id
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+    private fun setupDrawerLayout() {
 
-        // Relaciono mi Bottom Nav View con mi nav graph
-        bottomNavView.setupWithNavController(navHostFragment.navController)
+        val navController = navHostFragment.navController
+        // Vinculo la navegación del drawer con la del graph
+        navigationView.setupWithNavController(navController)
 
-        // Agrego un listener para poder escuchar cada vez que se realiza una navegacion
-        navHostFragment.navController.addOnDestinationChangedListener { _, destination, arguments ->
+        // Configuro la appbar para que muestre el icono del drawer y actualice el titulo
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
 
-            // Si mi destino es el login entonces oculto la barra inferior. Caso contrario la muestro
-            if (destination.id == R.id.loginFragment) {
-                bottomNavView.visibility = View.GONE
-            } else {
-                bottomNavView.visibility = View.VISIBLE
-
-                // Si mi destino es la Home, tomo el userName que recibio por parametro y lo almaceno en un Object
-                if (destination.id == R.id.homeFragment) {
-                    arguments?.getString("username")?.let { UserSession.userName = it }
-                }
-            }
+        // Listener para cuando se realiza la navegacion
+        navController.addOnDestinationChangedListener { _, _, _ ->
+            // Aca le digo que quiero que mi icono izquierdo de la appbar sea el del drawer
+        //    supportActionBar?.setHomeAsUpIndicator(R.drawable.hamburger)
         }
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        return false
+    }
+
 }
